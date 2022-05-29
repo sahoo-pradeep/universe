f583be38247efa1b52c789e9d1ad33eff1cc053c
Pradeep Kumar Sahoo <sahoo.pradeep.nits@gmail.com>
2022-05-29 23:53:54 +0530

    added passbook module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.0"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+//	id("org.springframework.boot") version "2.7.0"
+//	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+//	kotlin("plugin.spring") version "1.6.21"
 }
 
-group = "projects.sahoo"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+subprojects {
+	apply(plugin = "kotlin")
 
-repositories {
-	mavenCentral()
+	group = "projects.sahoo"
+	version = "0.0.1-SNAPSHOT"
+	java.sourceCompatibility = JavaVersion.VERSION_11
+
+	repositories {
+		mavenLocal()
+		mavenCentral()
+	}
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+//	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+//	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
