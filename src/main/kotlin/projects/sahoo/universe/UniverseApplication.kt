99aaf2a9ebbdb33b4dfe6a0421bc4e0b94566797
Pradeep Kumar Sahoo <sahoo.pradeep.nits@gmail.com>
2022-05-29 21:19:47 +0530

    init

---
+++ src/main/kotlin/projects/sahoo/universe/UniverseApplication.kt
@@ -0,0 +1,11 @@
+package projects.sahoo.universe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UniverseApplication
+
+fun main(args: Array<String>) {
+	runApplication<UniverseApplication>(*args)
+}
---
+++ src/test/kotlin/projects/sahoo/universe/UniverseApplicationTests.kt
@@ -0,0 +1,13 @@
+package projects.sahoo.universe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UniverseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
