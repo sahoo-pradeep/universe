d69dadd13b8ff3c7d07c9453b175634f26f19167
Pradeep Kumar Sahoo <sahoo.pradeep.nits@gmail.com>
2022-06-04 18:21:29 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-//	id("org.springframework.boot") version "2.7.0"
-//	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.21"
-//	kotlin("plugin.spring") version "1.6.21"
 }
 
 subprojects {
@@ -21,10 +18,8 @@ subprojects {
 }
 
 dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-//	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
